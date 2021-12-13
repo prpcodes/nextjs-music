86d012fb008d28395ee91e48371fec87c0cbfc93
prpwien <mail@prp.wien>
2021-12-13 18:06:21 +0100

    ✨added playerLayout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@chakra-ui/layout';
+
+const PlayerLayout = ({ children }) => {
+  return <Box>Layout {children}</Box>;
+};
+
+export default PlayerLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,42 @@
-import '../styles/globals.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import 'reset-css';
+import PlayerLayout from '../components/playerLayout';
 
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus:': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+});
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />{' '}
+      </PlayerLayout>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
