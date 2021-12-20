cb9bafd51e98d4cd145eca4ef2398d52b9f2da12
prpwien <mail@prp.wien>
2021-12-20 19:23:54 +0100

    ✨ added musicMenu

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,33 +7,48 @@ const navMenu = [
   {
     name: 'Home',
     icon: MdHome,
-    path: '/',
+    route: '/',
   },
   {
     name: 'Search',
     icon: MdSearch,
-    path: '/search',
+    route: '/search',
   },
   {
     name: 'Your Library',
     icon: MdLibraryMusic,
-    path: '/library',
+    route: '/library',
   },
 ];
+
+const musicMenu = [
+  {
+    name: 'Create Playlist',
+    icon: MdPlaylistAdd,
+    route: '/',
+  },
+  {
+    name: 'Favorites',
+    icon: MdFavorite,
+    route: '/favorites',
+  },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
 
 const Sidebar = () => {
   return (
     <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
-          <NextImage src="/logo.svg" width={120} height={60} />
+          <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
         <Box marginBottom="20px">
           <List spacing={2}>
             {navMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
                 <LinkBox>
-                  <NextLink href={menu.path} passHref>
+                  <NextLink href={menu.route} passHref>
                     <LinkOverlay>
                       <ListIcon as={menu.icon} color="white" marginRight="20px" />
                       {menu.name}
@@ -44,8 +59,39 @@ const Sidebar = () => {
             ))}
           </List>
         </Box>
+        <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" />
+        <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
 };
+
 export default Sidebar;
