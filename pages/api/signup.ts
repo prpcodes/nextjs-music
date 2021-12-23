02ba04613347c85e65c06f40d1e2f7014fee2a67
Philipp R. Proksch <mail@prp.wien>
2021-12-23 13:07:33 +0000

    ✨ created signup

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,9 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 
 import prisma from "../../lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const salt = bcrypt.genSaltSync();
