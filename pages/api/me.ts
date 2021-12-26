a28b60252413e5dae7ff9e8af4d8db84940815e9
Philipp R. Proksch <mail@prp.wien>
2021-12-26 21:08:17 +0000

    ✨ added playlistsCount

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,4 +1,10 @@
 import { validateRoute } from "../../lib/auth";
-export default validateRoute((req, res, user) => {
-  res.json(user);
+import prisma from "../../lib/prisma";
+
+export default validateRoute(async (req, res, user) => {
+  const playlistsCount = await prisma.playlist.count({
+    where: { userId: user.id },
+  });
+
+  res.json({ ...user, playlistsCount });
 });
