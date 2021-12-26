2653f70bc6506d6d0a605f5c21ac72c61c0b1f3b
Philipp R. Proksch <mail@prp.wien>
2021-12-26 21:32:58 +0000

    ✨ added validateToken

--- lib/auth.ts
+++ lib/auth.ts
@@ -33,3 +33,8 @@ export const validateRoute = (handler) => {
     res.json({ error: "Not Authorized!" });
   };
 };
+
+export const validateToken = (token) => {
+  const user = jwt.verify(token, "hello");
+  return user;
+};
