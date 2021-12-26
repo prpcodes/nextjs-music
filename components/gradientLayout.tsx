35e332c63212a60367eff4926500a3230729f0cf
Philipp R. Proksch <mail@prp.wien>
2021-12-26 20:13:39 +0000

    ✨ added Image and Texts

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -17,7 +17,23 @@ const GradientLayout = ({
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
     >
       <Flex bg={`${color}.600`} padding="40px" align="end">
-        <Box>Whatsup</Box>
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding="20px" lineHeight="40px" color="white">
+          <Text fontSize="small" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize="small" paddingTop="10px">
+            {description}
+          </Text>
+        </Box>
       </Flex>
     </Box>
   );
