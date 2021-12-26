0e7f2dfd80a2e0b07d236df29083ba897e02abcb
Philipp R. Proksch <mail@prp.wien>
2021-12-26 21:07:54 +0000

    ✨ added artistcards and user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,54 @@
-import Head from "next/head";
-import Image from "next/image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import GradientLayout from "../components/gradientLayout";
-import styles from "../styles/Home.module.css";
+import prisma from "../lib/prisma";
+import { Image } from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
-const Home = () => {
+const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
-    <GradientLayout color="red">
-      <div>Homepage</div>
+    <GradientLayout
+      color="gray"
+      subtitle="profile"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} Public Playlists`}
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      roundImage
+    >
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="sm">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box>
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 };
 
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
+};
 export default Home;
