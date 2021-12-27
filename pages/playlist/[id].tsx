f3692b1fe2bb50f6c53ab0433a15db7da9830919
Philipp R. Proksch <mail@prp.wien>
2021-12-27 11:28:07 +0000

    ✨ added SongTable

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,10 +1,40 @@
 import GradientLayout from "../../components/gradientLayout";
+import SongTable from "../../components/songsTable";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
-const Playlist = ({ playlist }) => {
-  return <div>{playlist?.name}</div>;
+//create random background color
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
 };
+
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <SongTable />
+    </GradientLayout>
+  );
+};
+
 export const getServerSideProps = async ({ query, req }) => {
   const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
   const [playlist] = await prisma.playlist.findMany({
