7db19a0e906211bb1b799708f508a6b57bfec455
Philipp R. Proksch <mail@prp.wien>
2021-12-27 12:46:39 +0000

    ✨ create playerLayout

---
+++ components/playerBar.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+
+const PlayerBar = () => {
+  return (
+    <Box height="100px" width="100vw" bg="gray.900" padding="10px">
+      <Flex align="center">
+        <Box padding="20px" color="white" width="30%">
+          <Text fontSize="lg">Song Name</Text>
+          <Text fontSize="sm">Artist Name</Text>
+        </Box>
+        <Box width="40%">controls</Box>
+        <Box width="30%">sound</Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default PlayerBar;
